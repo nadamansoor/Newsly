0db70bba0a888e8057f9b98837e6b835eeafc422
nadamansoor <nadamansour1566@gmail.com>
2026-04-22 21:54:39 +0200

    finish app

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,9 +1,16 @@
 class ArticleModel {
-  // img can be null = nullable
-  final String? image ;
-  final String? title ;
-  final String? subtitle ;
-  final String? url ;
-
+   final String? image ;
+   final String? title ;
+   final String? subtitle ;
+   final String? url ;
+// final is immutable
   ArticleModel({required this.image, required this.title, required this.subtitle, required this.url});
+
+  factory ArticleModel.fromJson(json) {
+   return ArticleModel(
+    image: json['image'], 
+   title: json['title'], 
+   subtitle: json['subtitle'],
+    url: json['url']);
+  }
 }
